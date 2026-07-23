5a9dd568a4da56caa74dbef2cfe3a4c06888ff90
wibuvpnstore <wibuvpnstore@gmail.com>
2026-07-24 02:34:41 +0800

    fix: add cache bypass to migrate-v2.sh downloads to avoid stale github CDN files

--- migrate-v2.sh
+++ migrate-v2.sh
@@ -20,9 +20,10 @@ echo -e "$LINE"
 
 REPO="https://raw.githubusercontent.com/WBVPN/WIBUTUNNEL/main"
 CONFIG="/usr/local/etc/xray/config.json"
+CACHE_BYPASS="t=$(date +%s)"
 
 echo -e "[1/4] Mengunduh dependensi wibu-utils.sh..."
-wget -qO /usr/local/bin/wibu-utils.sh "${REPO}/wibu-utils.sh"
+wget -qO /usr/local/bin/wibu-utils.sh "${REPO}/wibu-utils.sh?${CACHE_BYPASS}"
 chmod +x /usr/local/bin/wibu-utils.sh
 
 echo -e "[2/4] Patching Xray config.json untuk API Handler..."
@@ -54,23 +55,23 @@ fi
 
 echo -e "[3/4] Mengunduh ulang semua script core & menu..."
 cd /usr/local/bin
-wget -qO m-vless "${REPO}/menu/m-vless.sh"
-wget -qO m-vmess "${REPO}/menu/m-vmess.sh"
-wget -qO m-trojan "${REPO}/menu/m-trojan.sh"
-wget -qO m-setting "${REPO}/menu/m-setting.sh"
-wget -qO m-backup "${REPO}/menu/m-backup.sh"
-wget -qO bot-daemon "${REPO}/menu/bot-daemon.sh"
-wget -qO cek-trafik "${REPO}/menu/cek-trafik.sh"
-wget -qO menu-recovery "${REPO}/menu/menu-recovery.sh"
-wget -qO menu-lock "${REPO}/menu/menu-lock.sh"
-wget -qO menu-unlock "${REPO}/menu/menu-unlock.sh"
-wget -qO common.sh "${REPO}/common.sh"
+wget -qO m-vless "${REPO}/menu/m-vless.sh?${CACHE_BYPASS}"
+wget -qO m-vmess "${REPO}/menu/m-vmess.sh?${CACHE_BYPASS}"
+wget -qO m-trojan "${REPO}/menu/m-trojan.sh?${CACHE_BYPASS}"
+wget -qO m-setting "${REPO}/menu/m-setting.sh?${CACHE_BYPASS}"
+wget -qO m-backup "${REPO}/menu/m-backup.sh?${CACHE_BYPASS}"
+wget -qO bot-daemon "${REPO}/menu/bot-daemon.sh?${CACHE_BYPASS}"
+wget -qO cek-trafik "${REPO}/menu/cek-trafik.sh?${CACHE_BYPASS}"
+wget -qO menu-recovery "${REPO}/menu/menu-recovery.sh?${CACHE_BYPASS}"
+wget -qO menu-lock "${REPO}/menu/menu-lock.sh?${CACHE_BYPASS}"
+wget -qO menu-unlock "${REPO}/menu/menu-unlock.sh?${CACHE_BYPASS}"
+wget -qO common.sh "${REPO}/common.sh?${CACHE_BYPASS}"
 chmod +x *
 
 cd /usr/local/sbin
-wget -qO lock-user "${REPO}/sbin/lock-user"
-wget -qO unlock-user "${REPO}/sbin/unlock-user"
-wget -qO algojo-wibu "${REPO}/sbin/algojo-wibu"
+wget -qO lock-user "${REPO}/sbin/lock-user?${CACHE_BYPASS}"
+wget -qO unlock-user "${REPO}/sbin/unlock-user?${CACHE_BYPASS}"
+wget -qO algojo-wibu "${REPO}/sbin/algojo-wibu?${CACHE_BYPASS}"
 chmod +x *
 
 # Patch Wibu Daemon
